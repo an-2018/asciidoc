582878268c736c0fe736c7769dd321c26ec083af
anilson <anilsonmonteiro@gmail.com>
2021-04-21 19:05:56 -0100

    update kotlin path test

--- paths/Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ paths/Kotlin for Java Developers. Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -7,6 +7,17 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
     var rightPositions = 0
     var wrongPositions = 0
     var aux = StringBuilder()
+
+    fun findOccurencies(c:Char, word:String):Int{
+        var num = 0
+        for(char in word){
+            if(char == c){
+                num++
+            }
+        }
+        return num
+    }
+
     for(i in secret.indices){
 
         if(guess[i] !in secret){
@@ -18,10 +29,13 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
             rightPositions++
             continue
         }
-        println(guess[i])
+
         for(j in secret.indices){
             if(guess[j] == secret[j]) continue
-            if(aux.contains(guess[i])) continue
+            if(aux.contains(guess[i])){
+                continue
+            }
+            println(guess[i])
             if(guess[i] == secret[j] && i != j){
                 println("wrong")
                 aux.append(guess[i])
