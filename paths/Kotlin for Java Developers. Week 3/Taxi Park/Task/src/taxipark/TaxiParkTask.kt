3257996dbf7f003227af2706dba27a70a35fed30
anilson <anilsonmonteiro@gmail.com>
2021-04-23 08:37:41 -0100

    tips task kotlin

--- paths/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ paths/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -4,19 +4,28 @@ package taxipark
  * Task #1. Find all the drivers who performed no trips.
  */
 fun TaxiPark.findFakeDrivers(): Set<Driver> =
-        TODO()
+    this.allDrivers.filter {
+            driver -> driver !in (this.trips.map { it.driver })
+    }.toSet()
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
-fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-        TODO()
+fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> {
+    val exp = {pass:Passenger -> trips.map { it.passengers }.filter{pass in it}.count()}
+    println(exp(allPassengers.first()))
+    return this.allPassengers.filter{exp(it)>=minTrips}.toSet()
+}
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
-fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-        TODO()
+fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> {
+
+    val exp = {pass:Passenger, driv:Driver -> trips.groupBy (keySelector = {it.driver}, valueTransform ={it.passengers}).filter{pass in it.value && it.key == driv }.count()}
+    println(trips.map{it -> mapOf(it.driver to it.passengers)})
+    return this.allPassengers.filter{exp(it,driver) > 1}.toSet()
+}
 
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
