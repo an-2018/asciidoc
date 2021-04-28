d0d54ed1c5d6a48dc1bcf3cad693acbc6ff38877
anilson <anilsonmonteiro@gmail.com>
2021-04-28 08:22:43 -0100

    start week 4 test 2 kotlin

--- paths/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
+++ paths/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
@@ -1,7 +1,44 @@
 package board
 
 import board.Direction.*
+import java.lang.IllegalArgumentException
+import board.*
 
-fun createSquareBoard(width: Int): SquareBoard = TODO()
+fun createSquareBoard(width: Int): SquareBoard = object : SquareBoard{
+    override val width: Int = width
+    override fun getCellOrNull(i: Int, j: Int): Cell? {
+        return if(i > width || j > width) null
+        else getCell(i,j)
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        if(i <= 0 || j <= 0) throw IllegalArgumentException()
+        return Cell(i, j)
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        val list:ArrayList<Cell> = ArrayList()
+        for (i in 1..width){
+            for(j in 1..width){
+                list.add(Cell(i,j))
+            }
+        }
+        return list
+
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        TODO("Not yet implemented")
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        TODO("Not yet implemented")
+    }
+
+}
 fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
 
