225d95f03958289ad3af1a8a51389fb6d42984e1
anilson <anilsonmonteiro@gmail.com>
2021-05-04 14:36:35 -0100

    add flutter project

---
+++ paths/flutter_path/samples_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  MyApp({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context){
+      return Container(
+        child: Row(
+          children: <Widghet>[
+
+          ],
+        ),
+      )
+  }
+}
+
+class Product extends StatelessWidget {
+  Product();
+
+  @overr
+}
